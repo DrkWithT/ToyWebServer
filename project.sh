d844566cba22b851e1332c6c2c1e2122d8db0869
drkwitht <derektan3510@gmail.com>
2024-07-11 22:41:25 -0700

    Fix utility build scripts.

--- project.sh
+++ project.sh
@@ -9,9 +9,9 @@ fi
 choice="$1"
 
 if [[ "$choice" = "d" ]]; then
-    cmake --fresh -S . -B build -DDBG_BUILD:BOOL=1 && cmake --build build -- -j 2 && echo "Debug build OK!" || echo "Build failed, see logs!"
+    cmake --fresh -S . -B build -DDBG_BUILD:BOOL=1 && cmake --build build -- -j 2 && echo "Debug build OK!" && cp ./build/compile_commands.json . || echo "Build failed, see logs!"
 elif [[ "$choice" = "r" ]]; then
-    cmake --fresh -S . -B build -DDBG_BUILD:BOOL=0 && cmake --build build -- -j 2 && echo "Release build OK!" || echo "Build failed, see logs!"
+    cmake --fresh -S . -B build -DDBG_BUILD:BOOL=0 && cmake --build build -- -j 2 && echo "Release build OK!" && cp ./build/compile_commands.json . || echo "Build failed, see logs!"
 else
     echo "Usage: ./build.sh [d | r | i]\nOptions:\nd = debug build\nr = release build\ni = usage info"
 fi
